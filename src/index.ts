d48ea266860fdd9b49db00434404337eafc07409
Christian Fürst <c.fuerst@gmail.com>
2022-11-12 02:10:49 +0100

    chore(code): remove unused code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
-export interface TokenAuthorizerJwtFunctionProps extends lambda.FunctionOptions {
-  readonly tracingOption?: lambda.Tracing;
-}
-
 export class TokenAuthorizerJwtFunction extends lambda.Function {
   public constructor(scope: Construct, id: string, props: lambda.FunctionOptions) {
 
