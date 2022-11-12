8701cb9d61ede67471bbd9a2c7dd1e52d00ae31c
Christian Fürst <c.fuerst@gmail.com>
2022-11-12 03:57:06 +0100

    Update .projenrc.js

--- .projenrc.js
+++ .projenrc.js
@@ -32,7 +32,13 @@ const project = new awscdk.AwsCdkConstructLibrary({
   dotnet: {
     dotNetNamespace: 'CloudyWithAchanceOfMeatballs.CdkLambdaTokenAuthorizerJwt',
     packageId: 'CloudyWithAchanceOfMeatballs.CdkLambdaTokenAuthorizerJwt',
-  }
+  },
+  publishToMaven: {
+    mavenGroupId: 'io.github.cloudy-with-a-chance-of-meatballs',
+    javaPackage: 'io.github.cloudy-with-a-chance-of-meatballs.CdkLambdaTokenAuthorizerJwt',
+    mavenArtifactId: 'CdkLambdaTokenAuthorizerJwt',
+    mavenEndpoint: 'https://s01.oss.sonatype.org',
+  },
 });
 
 project.setScript('cdk', 'cdk');
