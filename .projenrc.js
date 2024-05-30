5649d3eeb3f6b9c161a59a6261184929fba0f7e5
cfuerst <c.fuerst@gmail.com>
2024-05-30 16:45:55 +0200

    chore(upgrades): general upgrades

--- .projenrc.js
+++ .projenrc.js
@@ -1,52 +1,53 @@
-const { awscdk, typescript, javascript } = require('projen');
+const { awscdk, typescript, javascript } = require("projen");
 
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'cfuerst',
-  authorAddress: 'c.fuerst@gmail.com',
-  cdkVersion: '2.5.0',
-  defaultReleaseBranch: 'main',
-  name: '@cloudy-with-a-chance-of-meatballs/cdk-lambda-token-authorizer-jwt',
-  repositoryUrl: 'https://github.com/cloudy-with-a-chance-of-meatballs/cdk-lambda-token-authorizer-jwt.git',
-  description: 'Add a lambda function to your aws-rest-api-gateway which can be used as a token authorizer',
-  stability: 'experimental',
-  license: 'MIT',
-  keywords: ['aws', 'cdk', 'lambda', 'apigateway', 'rest', 'api', 'jwt', 'tokenauthorizer', 'jwks', 'authorizer', 'token'],
-  deps: [
-    'aws-cdk-lib',
-    'constructs',
+  author: "cfuerst",
+  authorAddress: "c.fuerst@gmail.com",
+  cdkVersion: "2.5.0",
+  defaultReleaseBranch: "main",
+  name: "@cloudy-with-a-chance-of-meatballs/cdk-lambda-token-authorizer-jwt",
+  repositoryUrl:
+    "https://github.com/cloudy-with-a-chance-of-meatballs/cdk-lambda-token-authorizer-jwt.git",
+  description:
+    "Add a lambda function to your aws-rest-api-gateway which can be used as a token authorizer",
+  stability: "experimental",
+  license: "MIT",
+  keywords: [
+    "aws",
+    "cdk",
+    "lambda",
+    "apigateway",
+    "rest",
+    "api",
+    "jwt",
+    "tokenauthorizer",
+    "jwks",
+    "authorizer",
+    "token",
   ],
+  deps: ["aws-cdk-lib", "constructs"],
   npmAccess: javascript.NpmAccess.PUBLIC,
-  autoMerge: true,
-  autoApproveUpgrades: true,
-  autoApproveOptions: {
-    allowedUsernames: ['dependabot[bot]'],
-    secret: 'PROJEN_GITHUB_TOKEN',
-  },
   dependabot: true,
-  dependabotOptions: {
-    labels: ['auto-approve'],
-  },
   codeCov: true,
-  minNodeVersion: '16.18.1',
+  minNodeVersion: "16.18.1",
   python: {
-    distName: 'cloudy-with-a-chance-of-meatballs.cdk-lambda-token-authorizer-jwt',
-    module: 'cloudy_with_a_chance_of_meatballs_cdk_lambda_token_authorizer_jwt',
-    classifiers: [
-      'Programming Language :: Python :: 3.9',
-    ],
+    distName:
+      "cloudy-with-a-chance-of-meatballs.cdk-lambda-token-authorizer-jwt",
+    module: "cloudy_with_a_chance_of_meatballs_cdk_lambda_token_authorizer_jwt",
+    classifiers: ["Programming Language :: Python :: 3.9"],
   },
   jestOptions: {
     extraCliOptions: ['--testMatch "**/*.test.ts"'],
   },
-  gitignore: ['.idea/'],
-  npmignore: ['/function/', '!/assets/function/'],
+  gitignore: [".idea/"],
+  npmignore: ["/function/", "!/assets/function/"],
 });
 
-project.setScript('cdk', 'cdk');
+project.setScript("cdk", "cdk");
 
 new awscdk.LambdaFunction(project, {
-  entrypoint: 'function/src/authorizer.lambda.ts',
-  constructName: 'AuthorizerFunction',
+  entrypoint: "function/src/authorizer.lambda.ts",
+  constructName: "AuthorizerFunction",
   cdkDeps: project.cdkDeps,
   runtime: awscdk.LambdaRuntime.NODEJS_16_X,
 });
@@ -55,25 +56,25 @@ project.synth();
 
 const fn = new typescript.TypeScriptProject({
   parent: project,
-  outdir: 'function',
-  authorName: 'Christian Fürst',
-  authorEmail: 'c.fuerst@gmail.com',
+  outdir: "function",
+  authorName: "Christian Fürst",
+  authorEmail: "c.fuerst@gmail.com",
   authorOrganization: true,
-  license: 'MIT',
+  license: "MIT",
   name: `${project.name}`,
-  copyrightOwner: 'Christian Fürst',
-  defaultReleaseBranch: 'main',
+  copyrightOwner: "Christian Fürst",
+  defaultReleaseBranch: "main",
   deps: [
-    '@types/aws-lambda',
-    '@cloudy-with-a-chance-of-meatballs/aws-apigateway-rest-token-authorizer-jwt@^0.1.2',
-    'json5@^2.2.2',
+    "@types/aws-lambda",
+    "@cloudy-with-a-chance-of-meatballs/aws-apigateway-rest-token-authorizer-jwt@^0.1.5",
+    "json5@^2.2.2",
   ],
   eslint: true,
   prettier: true,
   prettierOptions: {
     settings: { printWidth: 120 },
   },
-  gitignore: ['src/authorizer-function.ts'],
+  gitignore: ["src/authorizer-function.ts"],
 });
 
 fn.synth();
